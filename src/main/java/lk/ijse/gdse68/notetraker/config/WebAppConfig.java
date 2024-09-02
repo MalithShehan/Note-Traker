d927966e680a58c8f5035a336a31db59f58dbfde
MalithShehan <shehansandakalum2003@gmail.com>
2024-09-02 10:52:22 +0530

    Add config in WebAppConfig for Multipart form data

--- src/main/java/lk/ijse/gdse68/notetraker/config/WebAppConfig.java
+++ src/main/java/lk/ijse/gdse68/notetraker/config/WebAppConfig.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse68.notetraker.config;
 
 
+import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "lk.ijse.gdse68.notetraker")
 @EnableWebMvc
 @EnableTransactionManagement
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "lk.ijse.gdse68.notetraker")
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2, //2MB
+        maxFileSize = 1024 * 1024 * 10, // 10MB
+        maxRequestSize = 1024 * 1024 * 50 // 50MB
+)
 public class WebAppConfig {
 }
+
