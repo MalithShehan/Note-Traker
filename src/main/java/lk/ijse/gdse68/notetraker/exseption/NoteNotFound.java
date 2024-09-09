e954ef32a8cc25f7917d3e9350489dfc438c26d7
MalithShehan <shehansandakalum2003@gmail.com>
2024-09-09 10:47:45 +0530

    Optimize update note process

---
+++ src/main/java/lk/ijse/gdse68/notetraker/exseption/NoteNotFound.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse68.notetraker.exseption;
+
+public class NoteNotFound extends RuntimeException{
+    public NoteNotFound() {}
+
+    public NoteNotFound(String message) {}
+
+    public void NoteNotFound(String message, Throwable cause) {}
+}
